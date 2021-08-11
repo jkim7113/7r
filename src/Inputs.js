3606c675dfceeda28431dbfd012ad211ece6eaf0
jkim7113 <jkim7113@gmail.com>
2021-08-11 12:46:00 -0500

    router update

--- src/Inputs.js
+++ src/Inputs.js
@@ -1,18 +1,96 @@
 import React from 'react';
-import './Inputs.css'
+import { Words } from './components/Words.js';
+import { Link } from 'react-router-dom';
 
-function Inputs() {
-    return(
-        <div className='cards'>
-        <div className='inputCardData'>
+class Inputs extends React.Component{
+    
+    state = {
+        wordInfo : [],
+        target : '',
+        isRequested : false,
+        word : '',
+        errorMsg : false
+      }
+    
+      async loadMeaning(){
+        try{
+          var res = await fetch(this.state.target);
+          var data = await res.json();
+          
+          if(data[0].meanings.length > 0){
+            var meanings = data[0];
+          }
+          else{
+            var meanings = data[1];
+          }
+          this.setState({wordInfo:meanings.meanings, isRequested:true, word:meanings.word});
+     
+        }
+        catch{
+          this.setState({isRequested:false, errorMsg:true},()=>{
+              console.error('An unexpected error occured');
+          });
+        }
+    
+      } 
+    
+        /*loadMeaning(){
+          return new Promise((resolve,reject)=>{
+            resolve(fetch(this.state.target))
+          });
+        }*/
+      render(){
+    
+        return(<div>
+          {this.state.isRequested ? (
+            this.props.history.push({pathname:`/${this.state.word}`, state: this.state})
+          ):(
+            <div className='cards'>
+            <div className='inputCardData'>
             <p className='announcement'>Type the word to search</p>
             <form>
                 <input autoComplete='off' id='textBox' type='text' size='30'></input>
             </form>
             <p className='errorMsg'>No words found</p>
+            </div>
+            </div>
+          ) }
         </div>
-        </div>
-    );
+        );
+      }
+    
+      componentDidMount(){
+
+        const form = document.querySelector('form');
+        const errorMsg = document.querySelector('.errorMsg'); 
+        
+        form.addEventListener('submit',(e)=>{
+          e.preventDefault();
+          var inputValue = document.querySelector('input').value;
+          var url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/'+inputValue.toLowerCase();
+    
+          this.setState({target:url},async()=>{
+            await this.loadMeaning();
+            /*await this.loadMeaning().then((res)=>{
+              return res = res.json();
+          }
+          ).then((res)=>{
+            if(data[0].meanings.length > 0){
+              var meanings = data[0];
+            }
+            else{
+              var meanings = data[1];
+            }
+            this.setState({wordInfo:meanings.meanings, isRequested:true, word:meanings.word});
+          });*/
+            if(this.state.errorMsg === true){
+              errorMsg.style.display = 'inline';
+            }
+            
+          });
+          });
+      }
+
 }
 
-export {Inputs};
+export default Inputs;
---
+++ src/components/Words.js
@@ -0,0 +1,148 @@
+import React, { useEffect } from 'react';
+import { Redirect } from 'react-router';
+import { PartOfSpeeches } from './PartOfSpeeches.js';
+import { Definitions } from './Defnitions.js';
+import { Examples } from './Examples.js';
+import './Words.css';
+
+function Words({history, match, location}){
+    
+    useEffect(()=>{
+
+      var partOfSpeechesArray = Array.prototype.slice.call(document.querySelectorAll('.partOfSpeech'));
+    
+        partOfSpeechesArray.map((word)=>{
+          word.addEventListener('click',()=>{
+    
+            var allWordDefs= document.querySelectorAll('.definitions');
+            var allWordClasses = document.querySelectorAll('.partOfSpeech');
+            var allWordExamples = document.querySelectorAll('.examples');
+    
+            for(var i=0;i<allWordDefs.length;i++){
+              allWordDefs[i].style.color = '#9baec8';
+            }
+            for(var i=0;i<allWordClasses.length;i++){
+              allWordClasses[i].style.color = '#9baec8';
+            }
+            for(var i=0;i<allWordExamples.length;i++){
+              allWordExamples[i].style.color = '#9baec8';
+            }
+    
+            var wordClass = word.classList[1];
+            var wordDefs = document.querySelectorAll(`.${wordClass}`); 
+            for(var i=0;i<wordDefs.length;i++){
+              wordDefs[i].style.color = 'white';
+            }
+            
+          });
+        });
+    },[]);
+
+    var sessionWord = window.sessionStorage.getItem('word');
+    var sessionWordInfo = JSON.parse(window.sessionStorage.getItem('wordInfo'));
+
+    if(location.state !== undefined){//검색해서 들어왔으면
+
+    const { word }  = match.params;
+    const { wordInfo } = location.state;
+    
+    if(sessionWord !== null){//세션에 뭔가있다면
+      window.sessionStorage.clear();
+      window.sessionStorage.setItem('word',word);
+      window.sessionStorage.setItem('wordInfo',JSON.stringify(wordInfo));
+    } else {//세션에 뭐가 없다면(첫번째)
+      window.sessionStorage.setItem('word',word);
+      window.sessionStorage.setItem('wordInfo',JSON.stringify(wordInfo));
+    }
+
+    sessionWord = window.sessionStorage.getItem('word');
+    sessionWordInfo = JSON.parse(window.sessionStorage.getItem('wordInfo'));
+    
+    return(//출력
+    <div className='words'>
+        <div className='wordCardData'>
+        <p className='word'>{sessionWord}</p>
+        {sessionWordInfo.map((currentWord,index)=>(
+          <span key ={index}>
+            <PartOfSpeeches partOfSpeech={currentWord.partOfSpeech}/>     
+            <span className='divider'>{(index < (sessionWordInfo.length-1))?(' | '):('')}</span>
+          </span>  
+        )
+        )
+        }
+        <ul>
+            {
+              sessionWordInfo.map((currentWord)=>(     
+                currentWord.definitions.map((currentDef,index)=>(
+                 <Definitions key={index} partOfSpeech={currentWord.partOfSpeech} definition={currentDef.definition}/>
+              ))
+              ))
+            }
+        </ul>
+        <ul>
+          {
+            sessionWordInfo.map((currentWord)=>(  
+              currentWord.definitions.map((currentEx,index)=>(
+               (currentEx.example)?(<Examples key={index} partOfSpeech={currentWord.partOfSpeech} example={currentEx.example}/>):(<li key={index} style={{display:'none'}}></li>)
+            ))
+            ))
+          }
+        </ul>
+
+        </div>
+        </div>
+    );
+    } else { ///:word 링크로 들어왔거나 앞으로 가기로 들어왔다면
+      if(sessionWord !== null){//앞으로 가기로 들어왔다면
+
+        sessionWord = window.sessionStorage.getItem('word');
+        sessionWordInfo = JSON.parse(window.sessionStorage.getItem('wordInfo'));
+
+        return(//출력
+          <div className='words'>
+              <div className='wordCardData'>
+              <p className='word'>{sessionWord}</p>
+              {sessionWordInfo.map((currentWord,index)=>(
+                <span key ={index}>
+                  <PartOfSpeeches partOfSpeech={currentWord.partOfSpeech}/>     
+                  <span className='divider'>{(index < (sessionWordInfo.length-1))?(' | '):('')}</span>
+                </span>  
+              )
+              )
+              }
+              <ul>
+                  {
+                    sessionWordInfo.map((currentWord)=>(     
+                      currentWord.definitions.map((currentDef,index)=>(
+                       <Definitions key={index} partOfSpeech={currentWord.partOfSpeech} definition={currentDef.definition}/>
+                    ))
+                    ))
+                  }
+              </ul>
+              <ul>
+                {
+                  sessionWordInfo.map((currentWord)=>(  
+                    currentWord.definitions.map((currentEx,index)=>(
+                     (currentEx.example)?(<Examples key={index} partOfSpeech={currentWord.partOfSpeech} example={currentEx.example}/>):(<li key={index} style={{display:'none'}}></li>)
+                  ))
+                  ))
+                }
+              </ul>
+      
+              </div>
+              </div>
+              
+          );
+
+      } else {///:word 링크로 들어왔다면
+
+        history.replace('/');
+        return false;
+
+      }
+
+    }
+
+}
+
+export {Words}
